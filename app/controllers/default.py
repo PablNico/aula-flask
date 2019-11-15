3b1ba42369f1bbe96b63873fce671a1c7b1b8413
PablNico <nico_camboriu@hotmail.com>
2019-11-15 14:25:24 -0300

    Initial commit

---
+++ app/controllers/default.py
@@ -0,0 +1,8 @@
+from app import app
+from flask import render_template
+
+@app.route("/index/<user>")
+@app.route("/", defaults={"user": None})
+def index(user):
+    return render_template("index.html", user=user)
+
---
+++ migrations/versions/d71e147d9344_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: d71e147d9344
+Revises: 
+Create Date: 2019-11-14 16:06:00.104390
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd71e147d9344'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
